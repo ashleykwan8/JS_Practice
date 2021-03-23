93639a2a21e9ccf4d6cb5df038f5fc3d61cd1d58
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-23 10:42:28 -0700

    practiced concatenation,length,toUpperCase(),toLowerCase(),Arithmetic logic

---
+++ practice.js
@@ -0,0 +1,15 @@
+// * IMPORTANT: COMPLETE WEB DEVELOPMENT 2021 UDEMY COURSE
+
+// TODO: Practice string concatenation, length, slicing, toUpperCase(), toLowerCase()
+// const askName = prompt("What is your name?");
+// const name = askName.slice(0,1).toUpperCase() + askName.slice(1,askName.length).toLowerCase();
+// const greeting = alert("Hello " + name + "!");
+
+// TODO: Practice Arithmetic and Modulo Operator
+// const dogAge = prompt("How old is your old?");
+// let dogYears = (dogAge - 2) * 4 + 21;
+// console.log(dogYears);
+
+// const ageAlert = alert("You dog is " + dogYears + " years old!");
+
+
