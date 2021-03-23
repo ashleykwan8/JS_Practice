d6ff528c53eac0df75ef6773d7c6c3133aef922c
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-23 15:03:05 -0700

    added coding exercise

--- practice.js
+++ practice.js
@@ -66,3 +66,18 @@
 //      turnRight();
 //   }
 
+
+// TODO: Life in Weeks Coding Exercise
+// function lifeInWeeks(age) {
+     
+        
+//         //Write your code here.
+//        const yearsRemaining = 90 - age;
+//        const days = yearsRemaining * 365;
+//        const weeks = yearsRemaining * 52;
+//        const months = yearsRemaining * 12;
+       
+//        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.") 
+        
+//     }
+
