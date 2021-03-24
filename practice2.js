0e9a9c47bcf18c49b10682ba1467bdeea05dce2c
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-24 08:20:34 -0700

    random number function practice

--- practice2.js
+++ practice2.js
@@ -1,1 +1,6 @@
 // * IMPORTANT: COMPLETE WEB DEVELOPMENT 2021 UDEMY COURSE
+// TODO: generate a random number (example 1 - 6)
+// let n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
