d43e70e360b4467a56f5d03ec19ce46ebc30b516
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-24 08:37:58 -0700

    added updated random num generator exercise

--- practice2.js
+++ practice2.js
@@ -4,3 +4,28 @@
 // n = n * 6;
 // n = Math.floor(n) + 1;
 // console.log(n);
+
+// TODO: Random Love Calculator Generator 
+// const yourName = prompt("What is your name?");
+// const companName = prompt("What is your companion's name?");
+
+// let loveScore = Math.random();
+// loveScore = loveScore * 100;
+// loveScore = Math.floor(loveScore) + 1;
+
+// alert(yourName + " and " + companName + " are " + loveScore + "% compatible.");
+
+// -----------------------------------
+// const yourName = prompt("What is your name?");
+// const companName = prompt("What is your companion's name?");
+
+// let n = Math.random();
+// n = n * 100;
+// n = Math.floor(n) + 1;
+
+// if (n > 70) {
+//     alert("Your love score is " + n + "%." + "You love eachother more than a cat loves catnip!")
+
+// } else {
+//     alert(yourName + " and " + companName + " are " + n + "% compatiable.")
+// }
