e00079d7118ab47dfaded1ea2a45fac286743178
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-24 09:25:28 -0700

    added Leap Year exercise

--- practice2.js
+++ practice2.js
@@ -16,16 +16,53 @@
 // alert(yourName + " and " + companName + " are " + loveScore + "% compatible.");
 
 // -----------------------------------
-// const yourName = prompt("What is your name?");
-// const companName = prompt("What is your companion's name?");
+// var yourName = prompt("What is your name?");
+// var companName = prompt("What is your companion's name?");
 
 // let n = Math.random();
 // n = n * 100;
 // n = Math.floor(n) + 1;
 
 // if (n > 70) {
-//     alert("Your love score is " + n + "%." + "You love eachother more than a cat loves catnip!")
+//     alert("Your love score is " + n + "%." + "You love each other more than a cat loves catnip!");
 
-// } else {
-//     alert(yourName + " and " + companName + " are " + n + "% compatiable.")
+// } 
+// if (n > 30 && n <= 70){
+//     alert("Your love score is " + n + "%." + "You love each other like milk and cookies!");
+
+// }
+// if (n <= 30) {
+//     alert("Your love score is " + n + "%." + "You love each other like oil and water!");
+
+// }
+
+// TODO: ADV. BMI Calculator 
+// function bmiCalculator (weight, height) {
+//     const bmi = weight / Math.pow(height, 2);
+//     if (bmi < 18.5) {
+//         return ("Your BMI is "+bmi+", so you are underweight.")
+//     }
+//     if (bmi>=18.5 && bmi<=24.9){
+//         return ("Your BMI is "+bmi+", so you have a normal weight.")
+//     }
+//     else {
+//         return ("Your BMI is "+bmi+", so you are overweight.")
+//     }
+
+//     return interpretation;
+// }
+
+// TODO: LEAP YEAR CALCULATOR
+// year that can be exactly divided by 4 (such as 2016, 2020, 2024, etc)
+// not	except if it can be exactly divided by 100, then it isn't (such as 2100, 2200, etc)
+// yes	except if it can be exactly divided by 400, then it is (such as 2000, 2400)
+
+// function isLeap(year) {
+// if(year % 4 === 0 || year % 400 === 0 && year % 100 !== 0) {
+//     return "Leap year."
+// }
+
+// else{
+//     return "Not leap year."
+// }
 // }
