ae79a0aeda1e8d9e677db056bfe991917d7952af
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-24 15:11:27 -0700

    Fizzbuzz challenge

--- practice2.js
+++ practice2.js
@@ -66,3 +66,22 @@
 //     return "Not leap year."
 // }
 // }
+// TODO: FizzBuzz Challenge
+// var output = [];
+// var count = 1
+// function fizzBuzz() {
+//     if (count % 3 === 0 && count % 5 === 0){
+//         output.push("fizzbuzz");
+//     }
+//     else if (count % 3 === 0){
+//         output.push("fizz");
+//     }
+//     else if (count % 5 === 0){
+//         output.push("buzz");
+//     }
+//     else {
+//         output.push(count);
+//     }
+//     console.log(output);
+//     count++;
+// }
