ba1994b5dc9b92fe2ce47e7cdcd7ce900539a2a8
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-25 08:06:32 -0700

    complete fibonacci sequence

--- practice2.js
+++ practice2.js
@@ -88,7 +88,7 @@
 
 //     }  
 // }
-// NOTE: UPDATED CODE
+// TODO: UPDATED CODE
 // var output = [];
 
 // function fizzBuzz() {
@@ -133,4 +133,20 @@
 //         beer--;
 //         }
 //     }
+// }
+// TODO: Fibonacci Sequence
+// function fibonacciGenerator(n) {
+//     var output = [];
+//     if (n===1){
+//         output = [0];
+//     }
+//     else if (n===2){
+//         output=[0,1]
+//     }
+//     else {
+//         output = [0,1];
+//         for(var i = 2;i<n;i++){
+//             output.push(output[output.length-2] + output[output.length - 1]);
+//         }
+//   } return output;
 // }
