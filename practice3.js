337dec1532b0ae4e7d7c22a78fe1086b165e7bf2
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-29 15:54:02 -0700

    higher functions practice

---
+++ practice3.js
@@ -0,0 +1,17 @@
+// TODO: Passing Functions as Arguments(Higher Order Functions)
+
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// operator is the function that it'll pass through 
+// function calculator(num1, num2, operator) {
+//     return operator(num1, num2);
+// }
+
+// calculator(4,5,add());
+// calculator(4,5,multiply());
